6e39c593a5753371aded909047ebd67f93a226a5
Muhammad Ibrahim <ibrahim@college.harvard.edu>
2015-04-17 04:11:21 -0400

    fixed map. removed graph. removed image. boxes are variable heights

--- assets/admin/pages/scripts/index3.js
+++ assets/admin/pages/scripts/index3.js
@@ -100,48 +100,48 @@ var Index = function() {
         },
 
         initCharts: function() {
-            if (Morris.EventEmitter) {
-                // Use Morris.Area instead of Morris.Line
-                dashboardMainChart = Morris.Area({
-                    element: 'sales_statistics',
-                    padding: 0,
-                    behaveLikeLine: false,
-                    gridEnabled: false,
-                    gridLineColor: false,
-                    axes: false,
-                    fillOpacity: 1,
-                    data: [{
-                        period: '2011 Q1',
-                        sales: 1400,
-                        profit: 400
-                    }, {
-                        period: '2011 Q2',
-                        sales: 1100,
-                        profit: 600
-                    }, {
-                        period: '2011 Q3',
-                        sales: 1600,
-                        profit: 500
-                    }, {
-                        period: '2011 Q4',
-                        sales: 1200,
-                        profit: 400
-                    }, {
-                        period: '2012 Q1',
-                        sales: 1550,
-                        profit: 800
-                    }],
-                    lineColors: ['#399a8c', '#92e9dc'],
-                    xkey: 'period',
-                    ykeys: ['sales', 'profit'],
-                    labels: ['Sales', 'Profit'],
-                    pointSize: 0,
-                    lineWidth: 0,
-                    hideHover: 'auto',
-                    resize: true
-                });
+            // if (Morris.EventEmitter) {
+            //     // Use Morris.Area instead of Morris.Line
+            //     dashboardMainChart = Morris.Area({
+            //         element: 'sales_statisticss',
+            //         padding: 0,
+            //         behaveLikeLine: false,
+            //         gridEnabled: false,
+            //         gridLineColor: false,
+            //         axes: false,
+            //         fillOpacity: 1,
+            //         data: [{
+            //             period: '2011 Q1',
+            //             sales: 1400,
+            //             profit: 400
+            //         }, {
+            //             period: '2011 Q2',
+            //             sales: 1100,
+            //             profit: 600
+            //         }, {
+            //             period: '2011 Q3',
+            //             sales: 1600,
+            //             profit: 500
+            //         }, {
+            //             period: '2011 Q4',
+            //             sales: 1200,
+            //             profit: 400
+            //         }, {
+            //             period: '2012 Q1',
+            //             sales: 1550,
+            //             profit: 800
+            //         }],
+            //         lineColors: ['#399a8c', '#92e9dc'],
+            //         xkey: 'period',
+            //         ykeys: ['sales', 'profit'],
+            //         labels: ['Sales', 'Profit'],
+            //         pointSize: 0,
+            //         lineWidth: 0,
+            //         hideHover: 'auto',
+            //         resize: true
+            //     });
 
-            }
+            // }
         },
 
         redrawCharts: function() {
