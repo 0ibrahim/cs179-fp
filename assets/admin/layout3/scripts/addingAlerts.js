5ab644663ed8e5cfa5ace586b70cef3f73b581ab
Muhammad Ibrahim <ibrahim@college.harvard.edu>
2015-04-17 03:06:02 -0400

    added alerts js

--- assets/admin/layout3/scripts/addingAlerts.js
+++ assets/admin/layout3/scripts/addingAlerts.js
@@ -2,4 +2,12 @@ function showAlert() {
 	toastr["warning"]("New Firefly was added. This is a dummy error.", "Firefly added")
 }
 
-showAlert();
+var listObject = $("#myHiddenList").clone();
+var visibleObject = listObject.removeClass("hidden")
+
+function addAlertToFeed() {
+	showAlert();
+	visibleObject.clone().prependTo("#myFeed").show("slow");	
+}
+
+setInterval(addAlertToFeed, 10000);
