82ebbaddac8e0846cd8625ac9e73564ece96d5fb
Karen Kennedy <kkennedy@college.harvard.edu>
2015-04-17 04:44:58 -0400

    spotlight added

--- assets/admin/layout3/scripts/globalsummary.js
+++ assets/admin/layout3/scripts/globalsummary.js
@@ -25,3 +25,31 @@ $("#day").click(function()  {
 	$('#countries').html("6");
 	$('#hospitals').html("32");
 });
+
+$("#salltime").click(function()  {
+	$('#sbabies').html("2,901");
+	$('#sactive').html("37/100");
+	$('#scountries').html("100");
+	$('#shospitals').html("5");
+});
+
+$("#syear").click(function()  {
+	$('#sbabies').html("1,200");
+	$('#sactive').html("70/93");
+	$('#scountries').html("20");
+	$('#shospitals').html("4");
+});
+
+$("#smonth").click(function()  {
+	$('#sbabies').html("75");
+	$('#sactive').html("60/89");
+	$('#scountries').html("7");
+	$('#shospitals').html("2");
+});
+
+$("#sday").click(function()  {
+	$('#sbabies').html("3");
+	$('#sactive').html("30/87");
+	$('#scountries').html("0");
+	$('#shospitals').html("1");
+});
