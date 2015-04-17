726bc6310dc53ad017664459a033d7c9d9a92f29
Karen Kennedy <kkennedy@college.harvard.edu>
2015-04-17 03:38:21 -0400

    global summary buttons and stats

---
+++ assets/admin/layout3/scripts/globalsummary.js
@@ -0,0 +1,27 @@
+$("#alltime").click(function()  {
+	$('#babies').html("780,901");
+	$('#active').html("309/500");
+	$('#countries').html("18");
+	$('#hospitals').html("100");
+});
+
+$("#year").click(function()  {
+	$('#babies').html("78,901");
+	$('#active').html("238/400");
+	$('#countries').html("9");
+	$('#hospitals').html("50");
+});
+
+$("#month").click(function()  {
+	$('#babies').html("6,575");
+	$('#active').html("200/400");
+	$('#countries').html("7");
+	$('#hospitals').html("45");
+});
+
+$("#day").click(function()  {
+	$('#babies').html("219");
+	$('#active').html("100/400");
+	$('#countries').html("6");
+	$('#hospitals').html("32");
+});
