a993129dc25936e9427a5fa798ae1d2839c87471
Muhammad Ibrahim <ibrahim@college.harvard.edu>
2015-04-25 15:23:59 -0400

    have three graphg

---
+++ assets/admin/layout3/scripts/dynamicGraphHumidity.js
@@ -0,0 +1,84 @@
+
+
+		// We use an inline dataHumidity source in the example, usually dataHumidity would
+		// be fetched from a server
+
+		var dataHumidity = [],
+		totalPoints = 300;
+
+		function getRandomHumidity() {
+
+			if (dataHumidity.length > 0)
+				dataHumidity = dataHumidity.slice(1);
+
+			// Do a random walk
+
+			while (dataHumidity.length < totalPoints) {
+
+				var prev = dataHumidity.length > 0 ? dataHumidity[dataHumidity.length - 1] : 50,
+					y = prev + Math.random() * 10 - 5;
+
+				if (y < 90) {
+					y = 90;
+				} else if (y > 110) {
+					y = 110;
+				}
+
+				dataHumidity.push(y);
+			}
+
+			// Zip the generated y values with the x values
+
+			var res = [];
+			for (var i = 0; i < dataHumidity.length; ++i) {
+				res.push([i, dataHumidity[i]])
+			}
+
+			return res;
+		}
+
+		// Set up the control widget
+
+		var updateInterval = 1000;
+		$("#updateInterval").val(updateInterval).change(function () {
+			var v = $(this).val();
+			if (v && !isNaN(+v)) {
+				updateInterval = +v;
+				if (updateInterval < 1) {
+					updateInterval = 1;
+				} else if (updateInterval > 2000) {
+					updateInterval = 2000;
+				}
+				$(this).val("" + updateInterval);
+			}
+		});
+
+		var plotHumidity = $.plot("#humidityGraph", [ getRandomHumidity() ], {
+			series: {
+				shadowSize: 0	// Drawing is faster without shadows
+
+			},
+			yaxis: {
+				min: 0,
+				max: 150,
+				ticks: 15,
+				axisLabel: "Humidity (Volts)",
+				labelWidth: 30
+			},
+			xaxis: {
+				show: false
+
+			}
+		});
+
+		function updateHumidty() {
+
+			plotHumidity.setData([getRandomHumidity()]);
+
+			// Since the axes don't change, we don't need to call plot.setupGrid()
+
+			plotHumidity.draw();
+			setTimeout(updateHumidty, updateInterval);
+		}
+
+		updateHumidty();
