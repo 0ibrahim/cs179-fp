19978d3e60cf9e790a9c9f16b049f574b8d4bf29
Muhammad Ibrahim <ibrahim@college.harvard.edu>
2015-04-25 18:01:28 -0400

    firefly details added

--- assets/admin/layout3/scripts/fireflies.js
+++ assets/admin/layout3/scripts/fireflies.js
@@ -1,3 +1,28 @@
 $('.newTable td:first-child').each(function(x){
 	$(this).wrapInner("<a href = '#basic' data-toggle = 'modal'> </a>");
 });
+
+
+
+$('.newTable td:first-child a').click(function(){
+	var info = []
+	console.log("this is running")
+	$(this).parent().parent().children('td').each(function(i){
+		if (i == 0) {
+			info[i] = $(this).find('a').html()
+		}else{
+			info[i] = $(this).html()
+			console.log(info[i]);	
+		}
+		
+	});
+
+	var fieldIds = ["serialNo", "hospital", "batchNo", "donor", "firstUse", "timeUsed", "status", "usage"];
+	console.log(info);
+
+	$.each(fieldIds, function(i){
+		$("#" + fieldIds[i]).html(info[i]);
+	});
+
+});
+
