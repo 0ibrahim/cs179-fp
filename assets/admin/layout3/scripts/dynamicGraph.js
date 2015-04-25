47270cf4c9ac0389f64f65cb29212db1eecc35e6
Muhammad Ibrahim <ibrahim@college.harvard.edu>
2015-04-25 14:44:29 -0400

    added graphs

---
+++ assets/admin/layout3/scripts/dynamicGraph.js
@@ -0,0 +1,89 @@
+
+
+		// We use an inline data source in the example, usually data would
+		// be fetched from a server
+
+		var data = [],
+		totalPoints = 300;
+		console.log("hellllo")
+
+		function getRandomData() {
+
+			if (data.length > 0)
+				data = data.slice(1);
+
+			// Do a random walk
+
+			while (data.length < totalPoints) {
+
+				var prev = data.length > 0 ? data[data.length - 1] : 50,
+					y = prev + Math.random() * 10 - 5;
+
+				if (y < 15) {
+					y = 15;
+				} else if (y > 45) {
+					y = 45;
+				}
+
+				data.push(y);
+			}
+
+			// Zip the generated y values with the x values
+
+			var res = [];
+			for (var i = 0; i < data.length; ++i) {
+				res.push([i, data[i]])
+			}
+
+			return res;
+		}
+
+		// Set up the control widget
+
+		var updateInterval = 1000;
+		$("#updateInterval").val(updateInterval).change(function () {
+			var v = $(this).val();
+			if (v && !isNaN(+v)) {
+				updateInterval = +v;
+				if (updateInterval < 1) {
+					updateInterval = 1;
+				} else if (updateInterval > 2000) {
+					updateInterval = 2000;
+				}
+				$(this).val("" + updateInterval);
+			}
+		});
+
+		var plot = $.plot("#temperatureGraph", [ getRandomData() ], {
+			series: {
+				shadowSize: 0	// Drawing is faster without shadows
+
+			},
+			yaxis: {
+				min: -20,
+				max: 80,
+				ticks: 7,
+				axisLabel: "Temperature (Celsius)",
+				labelWidth: 30
+			},
+			xaxis: {
+				show: false
+
+			}
+		});
+
+		function update() {
+
+			plot.setData([getRandomData()]);
+
+			// Since the axes don't change, we don't need to call plot.setupGrid()
+
+			plot.draw();
+			setTimeout(update, updateInterval);
+		}
+
+		update();
+
+		// Add the Flot version string to the footer
+
+		$("#footer").prepend("Flot " + $.plot.version + " &ndash; ");
